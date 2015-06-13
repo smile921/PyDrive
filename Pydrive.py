9c88a30f1332d59895036c1b8e9847814eef9219
jastra <jaredestroud@gmail.com>
2015-06-12 22:26:54 -0400

    Cleaned up some old code with some documentation and PEP 8 style-guides I've learned from over the years.

--- Pydrive.py
+++ Pydrive.py
@@ -1,37 +1,43 @@
 #!/usr/bin/env python
 #Author: Jared Stroud
-from scapy.all import *
-import os
-import time
-apList = [] 
+try:
+    from scapy.all import *
+    import os
+    import time
+except ImportError as err:
+    print("[ERROR] I'm missing:  " + str(err))
 
-interface = "wlan0"
+apList = [] 
+interface = "wlan0" #More than likely needs to be changed.
+
 f = open('wardriving.txt', 'a+') #Create/append to the file "wardriving.txt"
 
 #Put wireless NIC into monitor mode.
 def monMode():
-	os.system("ifconfig " + interface + " down")
-	os.system("iwconfig " + interface + " mode monitor")
+    os.system("ifconfig " + interface + " down")
+    os.system("iwconfig " + interface + " mode monitor")
 
 #Capture beacon frames and display MAC + SSID
 def pktCap(pkt):
 
-	if pkt.haslayer(Dot11Beacon):	
-		
-		if pkt.addr2 not in apList: #If we haven't seen this access point append it to the list(apList) and write to a file.
-			apList.append(pkt.addr2)
-			accessPoint = ("Access Point MAC address: " + pkt.addr2 + " with SSID " + pkt.info + "\n")
-			
-			print accessPoint
-			f.write(accessPoint)
-		else:
-			print("Waiting for something new...\n")
-			time.sleep(5)
-			
+    if pkt.haslayer(Dot11Beacon):	
+        if pkt.addr2 not in apList: #If we haven't seen this access point append it to the list(apList) and write to a file.
 
-monMode()
-	
-conf.iface = "mon0"
-sniff(prn=pktCap)
+            apList.append(pkt.addr2)
+            accessPoint = ("Access Point MAC address: " + str(pkt.addr2) + " with SSID " + str(pkt.info) + "\n")
+            print(accessPoint)
+            f.write(accessPoint)
 
-f.close() #We're done writing for today...
+        else:
+            print("Waiting for something new...\n")
+
+        time.sleep(5) # Take a second to avoid crazy writing to the file
+
+
+if "__name__" == __main__:
+    monMode()
+
+    conf.iface = "mon0"
+    sniff(prn=pktCap)
+
+    f.close() #We're done writing for today...
